489985fef3f1edf7037053ed4c26a53cebe96a59
Sebastian Wandzel <sebastianwandzel@yahoo.pl>
2024-10-17 18:09:56 +0200

    task 3

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 
 # Task 1
-
-# temperature=int(input("Podaj temperature w Kelwinach: "))
-# lNeutronow=int(input("Podaj liczbe neutronow na sekunde: "))
+'''
+temperature=int(input("Podaj temperature w Kelwinach: "))
+lNeutronow=int(input("Podaj liczbe neutronow na sekunde: "))
 
 def is_criticality_balanced(temperature, lNeutronow):
     if temperature<800 or lNeutronow>500 or temperature*lNeutronow<500000:
@@ -10,10 +10,11 @@ def is_criticality_balanced(temperature, lNeutronow):
     else :
         return False
 
-# print(is_criticality_balanced(temperature, lNeutronow))
-
+print(is_criticality_balanced(temperature, lNeutronow))
+'''
 
 # Task 2
+'''
 voltage=float(input("Podaj napiecie:"))
 current=float(input("podaj aktualna:"))
 theoretical_max_power=float(input("podaj teoretyczna maksymalna moc: "))
@@ -32,8 +33,23 @@ def reactor_efficiency(voltage, current, theoretical_max_power):
 
 print(reactor_efficiency(voltage,current,theoretical_max_power))
 
+'''
+# Task 3
+temperature=float(input("Podaj temperature"))
+neutronsPs=float(input("Podaj wartosc neutronow produkowanych na sekunde"))
+threshold=float(input("podaj prog: "))
 
 
+def fail_safe(temperature, neutronsPs, threshold):
+    criticality = temperature * neutronsPs
 
+    if criticality < 0.9 * threshold:
+        return "LOW"
+    elif 0.9 * threshold <= criticality <= 1.1 * threshold:
+        return "NORMAL"
+    else:
+        return "DANGER"
+
+print(fail_safe(temperature,neutronsPs,threshold))
 
 
