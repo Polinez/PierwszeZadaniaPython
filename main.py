eff659d51894f475fbeb363df4f357d6e2451491
Sebastian Wandzel <sebastianwandzel@yahoo.pl>
2024-10-17 16:38:00 +0200

    Taks 2

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 
-# zad 1
-temperature=int(input("Podaj temperature w Kelwinach: "))
-lNeutronow=int(input("Podaj liczbe neutronow na sekunde: "))
+# Task 1
+
+# temperature=int(input("Podaj temperature w Kelwinach: "))
+# lNeutronow=int(input("Podaj liczbe neutronow na sekunde: "))
 
 def is_criticality_balanced(temperature, lNeutronow):
     if temperature<800 or lNeutronow>500 or temperature*lNeutronow<500000:
@@ -9,4 +10,30 @@ def is_criticality_balanced(temperature, lNeutronow):
     else :
         return False
 
-print(is_criticality_balanced(temperature, lNeutronow))
+# print(is_criticality_balanced(temperature, lNeutronow))
+
+
+# Task 2
+voltage=float(input("Podaj napiecie:"))
+current=float(input("podaj aktualna:"))
+theoretical_max_power=float(input("podaj teoretyczna maksymalna moc: "))
+
+def reactor_efficiency(voltage, current, theoretical_max_power):
+    generated_power = voltage * current
+    efficiency=(generated_power/theoretical_max_power)*100
+
+    if efficiency>=80:
+        return "zielony"
+    elif efficiency>=60 and efficiency<80:
+        return "pomaranczowy"
+    elif efficiency>=30 and efficiency<60:
+        return "czerwony"
+    else: return "czarny"
+
+print(reactor_efficiency(voltage,current,theoretical_max_power))
+
+
+
+
+
+
